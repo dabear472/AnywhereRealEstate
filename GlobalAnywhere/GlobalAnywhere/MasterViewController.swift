4f634c53fb5743b691c7568bd38d412ea12d8c9f
dabear472 <jbuford72@gmail.com>
2023-04-27 22:03:14 -0500

    Began work on MasterViewController

---
+++ GlobalAnywhere/GlobalAnywhere/MasterViewController.swift
@@ -0,0 +1,25 @@
+//
+//  MasterViewController.swift
+//  GlobalAnywhere
+//
+//  Created by Jonathan Buford on 4/27/23.
+//
+
+import UIKit
+
+class MasterViewController: UITableView, UITableViewDelegate, UITableViewDataSource {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override var numberOfSections: Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+}
+
